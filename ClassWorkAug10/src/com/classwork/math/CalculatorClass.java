be0b47cdab4a1dcd4a0c128a6dee7f897aa91adb
MD JAMIL ANOWARI <jamil.anowari19@gmail.com>
2014-08-10 21:08:43 +0600

    Today Call work Aug 10, 2014

---
+++ ClassWorkAug10/src/com/classwork/math/CalculatorClass.java
@@ -0,0 +1,17 @@
+package com.classwork.math;
+
+public class CalculatorClass {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=6;
+		int c=9;
+		int d=8;
+		int result1=a/b;
+		int result2=c*d;
+		int finalResult=result2-result1;
+		System.out.println("The Result is:"+finalResult);
+
+	}
+
+}
---
+++ ClassWorkAug10/src/com/classwork/math/TodayClassWork.java
@@ -0,0 +1,12 @@
+package com.classwork.math;
+
+public class TodayClassWork {
+	public static void main (String [] args){
+	int firstNumber=15;
+	int secondNumber=3;
+	int add=5;
+	int total;
+	total = firstNumber-secondNumber+add;
+	System.out.println (total);
+}
+}
